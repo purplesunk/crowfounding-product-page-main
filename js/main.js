f99005891ed2afd7907ae12f0a3d63bac2fa2e82
Juan Ignacio Marín <ignacio.bendwise@aleeas.com>
2021-07-04 18:40:54 -0300

    finished?

--- js/main.js
+++ js/main.js
@@ -5,23 +5,24 @@ const body = document.querySelector('body')
 const bookmark = document.querySelector('.bookmark')
 const radioInputs = document.querySelectorAll('[name="reward"]')
 
-
-
-
 // Buttons functionality
-
 openModalButtons.forEach(button => button.addEventListener('click', _ => {
   const modal = document.querySelector(button.dataset.modalTarget)
-
-  if (button.dataset.modalTarget === "#navbar-menu") {
-    openNavbarMenu(modal)
-  } else if (button.dataset.index) {
-    openModal(modal)
-    selectRadio(button.dataset.index)
-  } else {
-    openModal(modal)
-  }
+  openModal(modal)
+  if (button.dataset.modalTarget === "#navbar-menu") { openNavbarMenu(modal) }
+  if (button.dataset.index) { selectRadio(button.dataset.index) }
 }))
+
+const openModal = modal => {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+  body.classList.add('modal-open')
+}
+const openNavbarMenu = modal => {
+  modal.closest('nav').classList.add('active')
+}
+
 
 closeModalButtons.forEach(button => button.addEventListener('click', event => {
   if (button.dataset.modalClose === "#navbar-menu") {
@@ -40,18 +41,6 @@ closeModalButtons.forEach(button => button.addEventListener('click', event => {
   }
 }))
 
-const openModal = modal => {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-  body.classList.add('modal-open')
-}
-
-const openNavbarMenu = modal => {
-  openModal(modal)
-  modal.closest('nav').classList.add('active')
-}
-
 const closeModal = (modal, check = false) => {
   if (modal == null) return
   modal.classList.remove('active')
@@ -62,15 +51,14 @@ const closeModal = (modal, check = false) => {
     body.classList.remove('modal-open')
   }
 }
-
 const closeNavbarMenu = (modal, navbar) => {
   closeModal(modal)
   navbar.classList.remove('active')
 }
 
+
 bookmark.addEventListener('click', event => {
   const button = event.currentTarget 
-
   button.classList.toggle('bookmarked')
 })
 
@@ -78,21 +66,11 @@ const selectRadio = index => {
   radioInputs[index].click()
 }
 
-
 // Radio functionality
-
 radioInputs.forEach(button => button.addEventListener('click', _ => {
   radioInputs.forEach(radio => {
     const pledgeDiv = radio.parentElement.nextElementSibling
-
-    if (radio.checked) {
-      pledgeDiv.classList.add('active')
-    } else {
-      pledgeDiv.classList.remove('active')
-    }
+    pledgeDiv.classList.remove('active')
+    if (radio.checked) { pledgeDiv.classList.add('active') }
   })
 }))
-
-const sendMoney = (quantity) => {
-  console.log(quantity);
-}
